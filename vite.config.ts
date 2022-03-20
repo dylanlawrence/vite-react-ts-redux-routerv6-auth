67f881b133609a998a7c0bab71020b6867b393bb
Dylan Lawrence <dylan@visualidentitygroup.com>
2022-03-19 20:49:03 -0700

    latest

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,7 @@
-import * as path from "path";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import rollupReplace from "@rollup/plugin-replace";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicDir: 'public',
-  plugins: [
-    rollupReplace({
-      preventAssignment: true,
-      values: {
-        __DEV__: JSON.stringify(true),
-        "process.env.NODE_ENV": JSON.stringify("development"),
-      },
-    }),
-    react(),
-  ],
-  resolve: process.env.USE_SOURCE
-      ? {
-        alias: {
-          "react-router": path.resolve(
-              __dirname,
-              "../../packages/react-router/index.tsx"
-          ),
-          "react-router-dom": path.resolve(
-              __dirname,
-              "../../packages/react-router-dom/index.tsx"
-          ),
-        },
-      }
-      : {},
-});
+  plugins: [react()]
+})
